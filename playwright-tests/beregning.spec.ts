7da81eb72681185721928714eecb3d2c7d0695d7
bjornko <bjorn.vaggen.konestabo@nav.no>
2025-07-14 15:30:17 +0200

    TOB-5150 debuggging playwright

--- playwright-tests/beregning.spec.ts
+++ playwright-tests/beregning.spec.ts
@@ -3,6 +3,9 @@ import { test } from "@playwright/test";
 test.describe("Beregning", () => {
   test("Enkel beregning tabelltrekk 8130", async ({ page }) => {
     await page.goto("http://localhost:5173/dare/form");
+    await page.goto("http://localhost:5173/dare/form");
+    await page.goto("http://localhost:5173/dare/form"); // desperate workaround.
+
     await page.waitForSelector("#select-rj", { state: "visible" });
     const select = page.locator("#select-rj");
     await select.selectOption("8130");
