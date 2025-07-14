c491ae5cfd24504008b513c0e9c5b2d14dd4193e
bjornko <bjorn.vaggen.konestabo@nav.no>
2025-07-14 16:55:37 +0200

    TOB-5150 debugging playwright

--- playwright-tests/beregning.spec.ts
+++ playwright-tests/beregning.spec.ts
@@ -4,8 +4,10 @@ test.describe("Beregning", () => {
   test("Enkel beregning tabelltrekk 8130", async ({ page }) => {
     await page.goto("/dare/form", { waitUntil: "load" });
     await page.waitForLoadState("networkidle");
-    const select = page.locator("#select-rj");
-    await select.selectOption("8130");
+
+    //const select = page.locator("#select-rj");
+    //await select.selectOption("8130");
+
     await page.getByRole("spinbutton", { name: "Sats", exact: true }).click();
     await page
       .getByRole("spinbutton", { name: "Sats", exact: true })
